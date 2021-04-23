618f34f473a9ae37090df1059de9bab9f97aa5cd
Albert Solana <iamtxena@gmail.com>
2021-04-23 07:17:03 +0200

    create structure to return guessin rule and probs

--- qcd/typings/configurations.py
+++ qcd/typings/configurations.py
@@ -1,6 +1,20 @@
 """ Typings from all qcd module """
-from typing import List, TypedDict
+from typing import List, Tuple, TypedDict
 from ..configurations.configuration import ChannelConfiguration
+from enum import Enum
+
+
+class Eta(Enum):
+    ETA0 = 0
+    ETA1 = 1
+    ETA2 = 2
+
+
+class MeasuredStatesEtaAssignment(TypedDict):
+    state_00: Eta
+    state_01: Eta
+    state_10: Eta
+    state_11: Eta
 
 
 class OptimalConfiguration(TypedDict):
@@ -17,6 +31,9 @@ class OptimalConfigurations(TypedDict, total=False):
     configurations: List[ChannelConfiguration]
     number_calls_made: List[int]
     legacy: bool
+    validated_probabilities: List[float]
+    eta_probabilities: List[Tuple[float, float, float]]
+    measured_states_eta_assignment: List[MeasuredStatesEtaAssignment]
 
 
 class TheoreticalOneShotOptimalConfiguration(OptimalConfiguration):
